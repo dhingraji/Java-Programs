74cb9f8e6c1f78ecc25b8901f293fb69de4a4dc8
dhingraji <103188060+dhingraji@users.noreply.github.com>
2023-04-12 08:36:13 -0700

    Update Static.java

--- Static.java
+++ Static.java
@@ -1,9 +1,24 @@
-
 public class Static {
+		   static int x = 10;
+		   static int y;
+		   static void func(int z) {
+		      System.out.println("x = " + x);
+		      System.out.println("y = " + y);
+		      System.out.println("z = " + z);
+		   }
+		   static {
+		      System.out.println("Running static initialization block.");
+		      y = x + 5;
+		   }
+		  
 
 	public static void main(String[] args) {
+		func(8);
 		// TODO Auto-generated method stub
 
 	}
 
 }
+
+
+	
